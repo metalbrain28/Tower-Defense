6982a1aa848b8a4740e2426bca6f6cca9e0f4e02
redPrick <coriiudavid@gmail.com>
2017-10-30 14:22:34 +0200

    Defense drag&drop

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    // Use this for initialization
+    //private BoxCollider2D boxCollider;
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnCollisionEnter2D(Collision2D collision) {
+        Debug.Log("collisoin detected");
+  
+    }
+}
---
+++ Assets/Scripts/TowerButton.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TowerButton : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+    [SerializeField]
+    private Tower prefabToSpawn = null;
+    private Tower instance = null;
+    private BoxCollider2D boxCollider;
+    private bool okToPlace = true;
+
+    public void OnBeginDrag(PointerEventData eventData) {
+        if (prefabToSpawn != null) {
+            instance = Instantiate<Tower>(prefabToSpawn);
+        }
+    }
+
+    public void OnDrag(PointerEventData data) {
+        if (instance != null) {
+            Vector3 wsPosition = Camera.main.ScreenToWorldPoint(data.position);
+            //wsPosition.x = (int)wsPosition.x + 0.5f;
+            //wsPosition.y = (int)wsPosition.y - 0.5f;
+            wsPosition.z = 0.5f;
+            instance.transform.position = wsPosition;
+
+            boxCollider = instance.GetComponent<BoxCollider2D>();
+            Collider2D[] overlap = Physics2D.OverlapAreaAll(boxCollider.bounds.min, boxCollider.bounds.max);
+            if (overlap.Length > 1) {
+                instance.GetComponent<Renderer>().material.color = Color.red;
+                okToPlace = false;
+            } else {
+                instance.GetComponent<Renderer>().material.color = Color.white;
+                okToPlace = true;
+            }
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData) {
+        //Debug.Log(okToPlace);
+        //if (instance != null && TowersManager.Instance != null) {
+            if (okToPlace) {
+                TowersManager.Instance.Towers.Add(instance);
+                instance = null;
+            } else {
+                Destroy(instance.gameObject);
+            }
+            
+
+        //}
+    }
+
+
+}
