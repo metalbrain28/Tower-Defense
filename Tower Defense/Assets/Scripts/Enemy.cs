676b886b020080e7ff977afc999e37db17c85018
alexandracatrinescu <32979832+alexandracatrinescu@users.noreply.github.com>
2017-11-09 22:50:48 +0200

    enemies implementation
    enemy movement and healyh bar implementation

---
+++ Tower Defense/Assets/Scripts/Enemy.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+     public GameObject[] waypoints;
+     public float life ;
+     public float speed = 0.5f;
+     public int targetWaypoint;
+     public bool isBoss = false;
+    protected bool dead;
+    Animator animator;
+   /* public GameObject projectile;
+    public Vector2 velocity;
+    public Vector2 offset = new Vector2(0.2f, 0.3f);*/
+
+    // Use this for initialization
+    void Start () {
+         if (isBoss == true)
+             life = 200f;
+         else
+             life = 100f;
+        this.transform.position = waypoints[0].transform.position;
+        animator = gameObject.GetComponent<Animator>() as Animator;
+        animator.Play("goRight");
+        HealthBar.maxHealth = life;
+
+    }
+     
+    // Update is called once per frame
+    void Update () {
+        
+        float step = speed*Time.deltaTime ;
+        if (targetWaypoint == 12)
+            Killed();
+        else {
+            if (gameObject.transform.position.y < waypoints[targetWaypoint].transform.position.y)
+            {
+                animator.Play("goUp");
+            }
+            if (gameObject.transform.position.y > waypoints[targetWaypoint].transform.position.y)
+            {
+                animator.Play("goDown");
+            }
+            if (gameObject.transform.position.x < waypoints[targetWaypoint].transform.position.x)
+            {
+                animator.Play("goRight");
+            }
+          /*  if (isBoss)
+            {
+               GameObject go =  Instantiate(projectile, (Vector2)transform.position + offset * transform.localScale.x, Quaternion.identity);
+            }
+             */  
+
+            transform.position = Vector3.MoveTowards(transform.position, waypoints[targetWaypoint].transform.position, step);
+            if (gameObject.transform.position == waypoints[targetWaypoint].transform.position)
+                targetWaypoint++;
+
+        }
+        
+      
+    }
+
+    public event System.Action OnDeath;
+
+    
+	void Killed(){
+        //EnemyManager.Instance.enemies.Remove(this.gameObject);
+        dead = true;
+        if(OnDeath != null)
+        {
+            OnDeath();
+        }
+        animator.Play("die");
+        Destroy(this.gameObject);
+	
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+       
+        HealthBar.health -= 10f;
+        if(HealthBar.health <= 0)
+        {
+            animator.Play("die");
+        }
+    }
+}
---
+++ Tower Defense/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class WaveSpawner : MonoBehaviour
+{
+
+   
+    public GameObject testEnemyPrefab;
+    public int wavePoint;
+    public Enemy[] objects;
+    public Wave currentWave;
+    int currentWaveNumber=0;
+    public float timeLastEnemySpawned ;
+
+    int enemiesRemainToSpawn;
+    int enemiesRemainAlive;
+    int enemies;
+    float nextSpawnTime;
+     Enemy enemy;
+    
+
+   
+    private int enemiesSpawned = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        enemies = currentWave.enemyCount;
+        NextWave();
+        timeLastEnemySpawned = Time.time;
+
+    }
+    void NextWave()
+    {
+        currentWaveNumber++;
+     
+        Wave newWave = Instantiate(currentWave, transform.position, Quaternion.identity) as Wave;
+       
+        /*
+         daca ne aflam la un nivel multiplu de 5 si in primul waveSpawner
+         daca inamicul e boss se va randa unul singur
+         altfel numarul de inamici este cu 1 mai mare decat cel din valul anterior
+         numarul inamicilor din al doilea Spawner creste cu 2
+         */
+        if( wavePoint==1)
+        {
+            enemy = objects[UnityEngine.Random.Range(0, objects.Length - 1)];
+            if(currentWaveNumber%5 == 0 && currentWaveNumber != 0)
+            {
+                enemy = objects[objects.Length - 1];
+            }
+            if (enemy.isBoss == true)
+            {
+
+                enemies = 1;
+            }
+            else
+            {
+                enemies++;
+            }
+
+        }
+        else
+        {
+            enemy = objects[UnityEngine.Random.Range(0, objects.Length )];
+            enemies = enemies + 2;
+            
+        }
+
+        currentWave.enemyCount = enemies;
+           
+            enemiesRemainToSpawn = currentWave.enemyCount;
+            enemiesRemainAlive = enemiesRemainToSpawn;
+      
+    
+    }
+    // Update is called once per frame
+    void OnEnemyDeath()
+    {
+      
+        enemiesRemainAlive--;
+        if(enemiesRemainAlive == 0)
+        {
+            NextWave();
+        }
+    }
+        void Update()
+    {
+        
+        if(enemiesRemainToSpawn>0 && Time.time > nextSpawnTime)
+        {
+            enemiesRemainToSpawn--;
+            nextSpawnTime = Time.time + currentWave.spawnInterval;
+
+           Enemy gameObjEnm = Instantiate(enemy, transform.position, Quaternion.identity) as Enemy;
+            gameObjEnm.OnDeath += OnEnemyDeath;
+               
+        }
+    }
+}
